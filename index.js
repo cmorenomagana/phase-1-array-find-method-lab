5b1ccec5a99b8c8c6722ca18bac59b481601bc33
Cuitlahuatl <cmorenomagana@gmail.com>
2023-11-07 17:16:50 -0600

    Array Find Method Lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// code your solution here
+function superbowlWin(record) {
+    const found = record.find((element) => element.result === "W")
+    if (found === undefined) {
+        return undefined
+    }
+    return found.year
+}
